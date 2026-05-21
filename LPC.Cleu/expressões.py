ba42e836c57c4d4f016c149b1b2d7e58cfb0d735
Mattew <mateusacioly2@gmail.com>
2026-05-20 22:54:24 -0300

    .

---
+++ LPC.Cleu/expressões.py
@@ -0,0 +1,9 @@
+#letra A
+num = float(input("Digite um número: "))
+resultado_a = (num + num)/3
+print(f"O resultado da letra A é: {resultado_a}")
+
+#letra B
+num1 = float(input("Digite o primeiro número: "))
+resultado_b = num1 + (num1/2)
+print(f"O resultado da letra B é: {resultado_b}")
---
+++ LPC.Cleu/expresãoaritmetica.py
@@ -0,0 +1,2 @@
+resultado = ((2/3 - (5-3)) + 1) *5
+print(f"o resultaodo da expressão é: {resultado}")
